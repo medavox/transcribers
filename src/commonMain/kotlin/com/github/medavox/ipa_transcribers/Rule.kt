c3100f01a9c6e30b2d995f975da2e0c2223f4c92
Adam Howard <medavox2+github@gmail.com>
2020-05-13 12:24:46 +0100

    style(Rule): remove spaces between names and types

--- src/commonMain/kotlin/com/github/medavox/ipa_transcribers/Rule.kt
+++ src/commonMain/kotlin/com/github/medavox/ipa_transcribers/Rule.kt
@@ -5,7 +5,7 @@ package com.github.medavox.ipa_transcribers
  * and what that match should be replaced with.
  * You can also optionally specify how far along the input string to advance afterwards, so you can match characters
  * that you don't want to consume (look-ahead matching).*/
-class Rule(matcher: Regex, outputString: String, lettersConsumed: Int? = null)
+class Rule(matcher:Regex, outputString:String, lettersConsumed:Int? = null)
     :IRule(null, matcher, { s, _->s+outputString }, lettersConsumed) {
     constructor(match:String, outputString:String, lettersConsumed:Int? = null)
             :this(Regex(match), outputString, lettersConsumed)
