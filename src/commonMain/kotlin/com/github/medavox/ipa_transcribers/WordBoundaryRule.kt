3837f0f13307ab4edd31db0b5065a525e44af563
Adam Howard <medavox2+github@gmail.com>
2020-05-13 12:24:17 +0100

    feat: add new rule type: WordBoundaryRule

---
+++ src/commonMain/kotlin/com/github/medavox/ipa_transcribers/WordBoundaryRule.kt
@@ -0,0 +1,11 @@
+package com.github.medavox.ipa_transcribers
+
+/**Works like a normal [Rule], but also checks that the last character in the consumed string is not a word character.
+ *
+ * This is a workaround to simulate "\bword\b" patterns. */
+class WordBoundaryRule(matcher:Regex, outputString:String, lettersConsumed:Int? = null)
+    :IRule(Regex("(^|[^a-zA-Z_0-9])"), matcher, { s, _->s+outputString }, lettersConsumed) {
+    constructor(match:String, outputString:String, lettersConsumed:Int? = null)
+            :this(Regex(match), outputString, lettersConsumed)
+}
+
