bf2f786dc77a0a80e3042106cbb56bd8dd5d98fa
Adam Howard <achdisposable1+github@gmail.com>
2020-05-18 18:56:09 +0100

    rename Builder to RuleBuilder

--- src/commonMain/kotlin/com/github/medavox/transcribers/RuleBuilder.kt
+++ src/commonMain/kotlin/com/github/medavox/transcribers/RuleBuilder.kt
@@ -1,6 +1,6 @@
 package com.github.medavox.transcribers
 
-class Builder(private val unconsumedMatcher:Regex) {
+class RuleBuilder(private val unconsumedMatcher:Regex) {
     private var consumedMatcher:Regex? = null
     private var outputString:(soFar:String, theMatches:MatchGroupCollection) -> String = {s, _ -> s}
     private var lettersConsumed:((theMatches:MatchGroupCollection) ->Int)?=null
@@ -10,32 +10,32 @@ class Builder(private val unconsumedMatcher:Regex) {
         return BaseRule(consumedMatcher, unconsumedMatcher, outputString, lettersConsumed)
     }
 
-    fun afterWordBoundary():Builder {
+    fun afterWordBoundary():RuleBuilder {
         consumedMatcher = Regex("(^|[^a-zA-Z_0-9])")
         return this
     }
 
-    fun consumedMatch(consumedMatch:Regex):Builder {
+    fun consumedMatch(consumedMatch:Regex):RuleBuilder {
         this.consumedMatcher = consumedMatch
         return this
     }
-    fun consumedMatch(consumedMatch:String):Builder {
+    fun consumedMatch(consumedMatch:String):RuleBuilder {
         this.consumedMatcher = Regex(consumedMatch)
         return this
     }
-    fun outputString(outputString:(soFar:String, theMatches:MatchGroupCollection) -> String):Builder {
+    fun outputString(outputString:(soFar:String, theMatches:MatchGroupCollection) -> String):RuleBuilder {
         this.outputString = outputString
         return this
     }
-    fun outputString(outputString:String):Builder {
+    fun outputString(outputString:String):RuleBuilder {
         this.outputString = {s, m -> s+outputString}
         return this
     }
-    fun lettersConsumed(lettersConsumed:(theMatches:MatchGroupCollection) -> Int):Builder {
+    fun lettersConsumed(lettersConsumed:(theMatches:MatchGroupCollection) -> Int):RuleBuilder {
         this.lettersConsumed = lettersConsumed
         return this
     }
-    fun lettersConsumed(lettersConsumed:Int):Builder {
+    fun lettersConsumed(lettersConsumed:Int):RuleBuilder {
         this.lettersConsumed = {lettersConsumed}
         return this
     }
